d9ebd9c0927ceb020eabe081141863b6797d4cfc
Gustavo Holanda de Sousa <106280222+GustavoHolanda16@users.noreply.github.com>
2023-12-20 22:45:20 -0300

    fim

---
+++ js.js
@@ -0,0 +1,8 @@
+const ent1 = document.querySelector('#nome')
+const ent2 = document.querySelector('#tel')
+const ent3 = document.querySelector('#email')
+
+function alerta(){
+    const nome = ent1.value
+    alert(`Obrigado ${nome}!`)
+}
